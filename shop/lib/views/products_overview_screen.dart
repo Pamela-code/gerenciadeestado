fafd3ed4eb0e5ac7964eb338f80ce3f052e3e882
Pamela-code <pamela.souza-jpa@hotmail.com>
2021-07-26 17:50:38 -0300

    Filtrar favoritos

--- shop/lib/views/products_overview_screen.dart
+++ shop/lib/views/products_overview_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:shop/widgets/product_grid.dart';
+import '../widgets/product_grid.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
  
@@ -7,24 +14,28 @@ class ProductsOverviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  
+  final Products products = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Minha Loja"),
         actions: <Widget>[
           PopupMenuButton(
             icon: Icon(Icons.more_vert),
-            onSelected: (int selectedValue){
-              print(selectedValue);
+            onSelected: (FilterOptions selectedValue){
+              if (selectedValue == FilterOptions.Favorite){
+                products.showFavoriteOnly();
+              } else{
+                products.showAll();
+              }
             },
             itemBuilder: (_) => [
             PopupMenuItem(
               child: Text('Somente Favoritos'),
-            value: 0,
+            value: FilterOptions.Favorite,
             ),
             PopupMenuItem(
               child: Text('Todos'),
-            value: 1,
+            value: FilterOptions.All,
             )
 
           ])
